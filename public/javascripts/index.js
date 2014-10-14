d6a5fec0914cbae6908499742c0d1ef37a0936dc
pan.zc <pan.zc@vcooline.com>
2014-10-14 15:04:19 +0800

    beta logo

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -15,6 +15,117 @@
   }(window.jQuery)
 
   + function ($){
+  var attr = 'placeholder', nativeSupported = attr in document.createElement('input')
+
+  $.fn.placeholder = function (options){
+    return this.each(function (){
+      var $input = $(this)
+
+      if(typeof options === 'string') {
+        options = { text: options }
+      }
+
+      var opt = $.extend({
+        text: '',
+        style: {},
+        namespace: 'placeholder',
+        useNative: true,
+        hideOnFocus: true
+      }, options||{})
+
+      if(!opt.text) {
+        opt.text = $input.attr(attr)
+      }
+
+      if(!opt.useNative) {
+        $input.removeAttr(attr)
+      } else if(nativeSupported) {
+        // 仅改变文本
+        $input.attr(attr, opt.text)
+        return
+      }
+
+      var width = $input.width(), height = $input.height()
+      var box_style = ['marginTop', 'marginLeft', 'paddingTop', 'paddingLeft', 'paddingRight']
+
+      var show = function (){ $layer.show() }
+      var hide = function (){ $layer.hide() }
+      var is_empty = function (){ return !$input.val() }
+      var check = function (){ is_empty() ? show() : hide() }
+
+      var position = function (){
+        var pos = $input.position()
+        if(!opt.hideOnFocus) {
+          // 按鍵隱藏的情况，需要移動光標两像素
+          pos.left += 2
+        }
+        $layer.css(pos)
+        $.each(box_style, function (i, name){
+          $layer.css(name, $input.css(name))
+        })
+      }
+
+      var layer_style = {
+        color: 'gray',
+        cursor: 'text',
+        textAlign: 'left',
+        position: 'absolute',
+        fontSize: $input.css('fontSize'),
+        fontFamily: $input.css('fontFamily'),
+        display: is_empty() ? 'block' : 'none'
+      }
+
+      // create
+      var layer_props = {
+        text: opt.text,
+        width: width,
+        height: 'auto'
+      }
+
+      // 确保只绑定一次
+      var ns = '.' + opt.namespace, $layer = $input.data('layer' + ns)
+      if(!$layer) {
+        $input.data('layer' + ns, $layer = $('<div>', layer_props).appendTo($input.offsetParent()))
+      }
+
+      // activate
+      $layer
+        .css($.extend(layer_style, opt.style))
+        .unbind('click' + ns)
+        .bind('click' + ns, function (){
+          opt.hideOnFocus&&hide()
+          $input.focus()
+        })
+
+      $input
+        .unbind(ns)
+        .bind('blur' + ns, check)
+
+      if(opt.hideOnFocus) {
+        $input.bind('focus' + ns, hide)
+      } else {
+        $input.bind('keypress keydown' + ns, function (e){
+          var key = e.keyCode
+          if(e.charCode||(key >= 65&&key <= 90)) {
+            hide()
+          }
+        })
+          .bind('keyup' + ns, check)
+      }
+
+      // 由于 ie 记住密码的特性，需要监听值改变
+      // ie9 不支持 jq bind 此事件
+      $input.get(0).onpropertychange = check
+
+      position()
+      check()
+    })
+  }
+
+}(window.jQuery)
+
+
+  + function ($){
   var Scrollwhere = function (element, options){
     this.$element = $(element);
     this.options = $.extend({}, $.fn.scrollwhere, options);
@@ -88,17 +199,23 @@ $(function (){
     var $self = $(this);
     $self.toggleClass('close-me');
   });
+
+  $('.J-getcode').on('click', function (){
+    getcode(this)
+  });
+
   function getcode(e){
-    var $self = $(e), count = $self.data('count')||30,
-      Interval = setInterval(function (){
-        if(count--) {
-          $self.text('获取验证码 ' + count.toString()).addClass('disabled');
-        }
-        else {
-          $self.text('获取验证码').removeClass('disabled');
-          clearInterval(Interval);
-        }
-      }, 1000);
+    var $self = $(e), count = $self.data('count')||30;
+    $self.text('获取验证码 ' + count.toString()).addClass('disabled');
+    var Interval = setInterval(function (){
+      if(count--) {
+        $self.text('获取验证码 ' + count.toString()).addClass('disabled');
+      }
+      else {
+        $self.text('获取验证码').removeClass('disabled');
+        clearInterval(Interval);
+      }
+    }, 1000);
   }
 
   $(document).scroll(function (){
@@ -115,24 +232,7 @@ $(function (){
 
   if($.browser.msie&&$.browser.version < 9) {
     $('.warning').removeClass('hide');
-    console.log('111')
   }
+  $('input[placeholder]').placeholder();
 
-
-  $.scrollUp({
-    scrollName: 'scrollUp',      // Element ID
-    scrollDistance: 300,         // Distance from top/bottom before showing element (px)
-    scrollFrom: 'top',           // 'top' or 'bottom'
-    scrollSpeed: 300,            // Speed back to top (ms)
-    easingType: 'easeOutCirc', // Scroll to top easing (see http://easings.net/)
-    animation: 'fade',           // Fade, slide, none
-    animationSpeed: 200,         // Animation speed (ms)
-    scrollTrigger: false,        // Set a custom triggering element. Can be an HTML string or jQuery object
-    scrollTarget: false,         // Set a custom target element for scrolling to. Can be element or number
-    scrollText: '<i class="icon-arrow-up"></i>', // Text for element, can contain HTML
-    scrollTitle: '滚动到顶部',          // Set a custom <a> title if required.
-    scrollImg: false,            // Set true to use image
-    activeOverlay: false,        // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-    zIndex: 2147483647           // Z-Index for the overlay
-  });
 });
