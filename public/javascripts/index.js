9a63e20fe3f20bb23380b24b2b3a249d5b9294e4
pan.zc <pan.zc@vcooline.com>
2014-11-05 15:56:35 +0800

    fix bugs

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -379,13 +379,13 @@ $(function (){
     trigger: "hover",
     title: '没有收到验证码怎么办？',
     html: true,
-    content: '<p>亲爱的用户，验证码短信/邮件正常都会在数秒钟内发送，如果您未收到短信/邮件，请参照如下常见情况进行解决：</p>' +
+    content: '<p>亲爱的用户，验证码短信正常都会在数秒钟内发送，如果您未收到短信/邮件，请参照如下常见情况进行解决：</p>' +
       '<p>1、由于您的手机或邮箱软件设定了某些安全设置，验证码短信/邮件可能被拦截进了垃圾箱。请打开垃圾箱查看，并将销售易号码添加为白名单。</p>' +
       '<p>2、由于运营商通道故障造成了短信/邮件发送时间延迟，请耐心稍候片刻或点击重新获取验证码。</p>' +
       '<p>3、关于手机号验证，目前支持移动、联通和电信的所有号码，暂不支持国际及港澳台地区号码。</p>' +
       '<p>如果您尝试了上述方式后均未解决，或存有其他疑问，请通过热线电话4000-365-711获取客户协助。</p>',
     template: '<div class="popover w-520px" role="tooltip" ><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>'
-  })
+  });
 
   $(document).scroll(function (){
     var $header = $('.header');
@@ -403,4 +403,6 @@ $(function (){
     $('.warning').removeClass('hide');
   }
   $('input[placeholder]').placeholder();
+  var gHeight=window.screen.height;
+  $('.g_art').css('min-height',gHeight-860);
 });
